16326af4a40f10164b95b445864cb560c253845e
Frank McAuley <frank.mcauley@gmail.com>
2020-09-25 14:02:05 -0400

    HashTable
    FM - Driving in changes with tests.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

--- HashTable/HashTable.swift
+++ HashTable/HashTable.swift
@@ -10,16 +10,46 @@ import Foundation
 public class HashTable <T> {
     private var buckets: Array<T?>
     private var slots: Int = 0
+    private var count = 0
+    
+    var keyCount : Int {
+        get {
+            count
+        }
+    }
     
     var capacity : Int {
         get {
-            0
+            slots
         }
     }
     
     
-    public init(capacity: Int = 20) {
+    public init(capacity: Int = 16) {
         self.buckets = Array<T?>(repeatElement(nil, count: capacity))
         self.slots = buckets.capacity
     }
+    
+    func insert(key k: T) {
+        let index = preHash(key: k, andCapacity: capacity)
+        buckets.insert(k, at: index)
+        count += 1
+    }
+    
+    func contains(key k: T) -> Bool {
+        let index = preHash(key: k, andCapacity: capacity)
+        return (buckets[index] != nil)
+    }
+    
+    
+    //this should be private and not exposed through interface
+    func preHash(key k: T, andCapacity m: Int) -> Int {
+        let a = 59389690744 //random number
+        let b = 30762909936 //random number
+        let p = 87178291199 //large prime fits with in the range of an unsigned integer
+        let kValue = k as! Int // I don't like this
+        
+        return (((a*kValue + b) % p) % m)
+    }
 }
+
--- HashTableTests/HashTableTests.swift
+++ HashTableTests/HashTableTests.swift
@@ -23,5 +23,57 @@ class HashTableTests: XCTestCase {
         let hashSet = HashTable<Int>(capacity: value)
         XCTAssertEqual(hashSet.capacity, value)
     }
+    
+    func testThatHashTableCanHashAKey() {
+        let value = 32
+        let hashSet = HashTable<Int>(capacity:value)
+        let key = 2
+        let capacity = hashSet.capacity
+        
+        let expected = testPreHash(key: key, andCapacity: capacity)
+        print(expected)
+        XCTAssertEqual(expected, hashSet.preHash(key: key, andCapacity: capacity))
+        
+    }
+    
+    func testThatTheCountOfTheHashSetWillBeOneAfterAnInsert() {
+        let value = 32
+        let hashSet = HashTable<Int>(capacity: value)
+        let key = 23
+        let expected = 1
+        hashSet.insert(key: key)
+        XCTAssertEqual(hashSet.keyCount, expected)
+    }
+    
+    func testThatTheBucketContainsaValueThatWasInserted() {
+        let value = 32
+        let hashSet = HashTable<Int>(capacity: value)
+        let key = 23
+        let expected = true
+        t
+        hashSet.insert(key: key)
+        let output = hashSet.contains(key: key)
+        XCTAssertEqual(output, expected)
+    }
+    
+    func testThatTheBucketDoesNotContainsaValueThatWasInserted() {
+        let value = 32
+        let hashSet = HashTable<Int>(capacity: value)
+        let key = 23
+        let expected = false
+        let output = hashSet.contains(key: key)
+        
+        XCTAssertEqual(output, expected)
+    }
+    
+    
+    // HELP FUNCTION for testing hashing
+    private func testPreHash(key k: Int, andCapacity m: Int) -> Int {
+        let a = 59389690744 //random number
+        let b = 30762909936 //random number
+        let p = 87178291199 //large prime fits with in the range of an unsigned integer
+        
+        return (((a*k + b) % p) % m)
+    }
 
 }
