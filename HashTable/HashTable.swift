f61be79cea0bf7d92348de9d79c4e1f642e227c0
Frank McAuley <frank.mcauley@gmail.com>
2020-09-25 12:52:27 -0400

    Update with files and status

--- HashTable/HashTable.swift
+++ HashTable/HashTable.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public class HashTable <T> {
+    private var buckets: Array<T?>
+    private var slots: Int = 0
+    
+    var capacity : Int {
+        get {
+            0
+        }
+    }
+    
+    
+    public init(capacity: Int = 20) {
+        self.buckets = Array<T?>(repeatElement(nil, count: capacity))
+        self.slots = buckets.capacity
+    }
+}
