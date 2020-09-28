c17d819eb9e60d757a28913378a62b1e0d70a864
Frank McAuley <frank.mcauley@gmail.com>
2020-09-28 15:15:57 -0400

    Looking for common characters
    Adding the source files and tests for the logic that will find common charactes.
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

---
+++ HashTable/FindCommonCharacters.swift
@@ -0,0 +1,17 @@
+//
+//  FindCommonCharacters.swift
+//  HashTable
+//
+//  Created by Frank McAuley on 9/28/20.
+//
+
+import Foundation
+
+class FindCommonCharacters {
+    func findCommonChars(_ arr: [String]) -> [String] {
+        guard arr.count > 0 else {
+            return []
+        }
+        return ["test"]
+    }
+}
---
+++ HashTableTests/FindCommonCharactersTests.swift
@@ -0,0 +1,33 @@
+//
+//  FindCommonCharactersTests.swift
+//  HashTableTests
+//
+//  Created by Frank McAuley on 9/28/20.
+//
+
+import XCTest
+
+class FindCommonCharactersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testThatFindCommonCharactersCanBeInitalized() throws {
+        let fcc = FindCommonCharacters()
+        XCTAssertNotNil(fcc)
+    }
+    
+    func testThatGivenAnEmptyArrayThefindDuplicateCharactersWillreturnAnEmptyArray() {
+        let fcc = FindCommonCharacters()
+        let input = [String]()
+        let output = fcc.findCommonChars(input)
+        XCTAssertEqual([], output)
+    }
+
+
+}
